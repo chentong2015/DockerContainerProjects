8f6542422e691478674278ad10dc4ce99973050a
chen tong <chen2015tong@gmail.com>
2024-01-03 12:30:42 +0100

    create docker project

---
+++ hello-docker-java/src/main/java/com/example/HelloDockerController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloDockerController {
+
+    @GetMapping("/hello/{name}")
+    public String helloDocker(@PathVariable(value = "name") String name) {
+        String response = "Hello " + name + " Response received on : " + new Date();
+        System.out.println(response);
+        return response;
+    }
+}
+
